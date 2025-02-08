f515338c11ce296ad49db9bb57365fdef27ed3eb
AARON MATHEW <aaronarackathara@gmail.com>
2025-02-08 19:09:40 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World! This is my updated message.</h1> {/* Changed text here */}
+    </div>
+  );
+}
+
+export default App;
