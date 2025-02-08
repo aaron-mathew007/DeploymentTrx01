a2e0c9e1d77b08f33ad3083303ea82d79ac0cc88
AARON MATHEW <aaronarackathara@gmail.com>
2025-02-08 19:09:59 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DeploymentsTracker from "./components/DeploymentsTracker";
+
+function App() {
+  return (
+    <div>
+      <h1>Deployment Tracker</h1>
+      <DeploymentsTracker />
+    </div>
+  );
+}
+
+export default App;
